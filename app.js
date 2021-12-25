b9bb7a59b2511ef8206c7f9362c11c290c717856
Ash7071 <96418821+Ash7071@users.noreply.github.com>
2021-12-25 11:27:00 +1100

    promise-demo

--- app.js
+++ app.js
@@ -1,49 +1,24 @@
-   /// express is a web framework ///
-   // it's acts like a middleware in nodejs ///
-   // endless the nodejs work as a server ///
-        // it take the request  and process the request and give back the response///
-        // command : npm i nodemon express --save ////
+//// multiple files access in promises  //// 
 
-        /// im creating server //////////////////////////////////
+const fs = require('fs');
 
-        const express = require('express');
+const util = require('util');
+
+var read= util.promisify(fs.readFile);
+
+    promise.all(
+        [
+          read('file1.txt'),
+          read('file2.txt'),
+          read('file3.txt'),
+          //read('note.txt')
+        ]
+    ).then((data)=>{
+            console.log(data);
+    }).catch((err)=>{
+      console.log(err);
+    }).finally(()=>{
+      console.log('read all the files')
+    })
+          
     
-        const app = express();
-
-        const port = 3000;
-
-        const fs = require('fs');
-
-        // app.get('/login', (req, res) => {
-
-        //     res.send('<h1>welcome to the login page</h1>');
-
-        // });
-
-        // app.get('/', (req, res) => {
-
-        //     res.send('<h1>Welcome to the express</h1>');
-        // });
-
-        app.get('/products', (request, response)=>{
-
-            fs.readFile('products.json',(err,data)=>{
-
-                if(err){
-                    throw err;
-                } else {
-                       response.send(JSON.parse(data));
-                }
-                
-            });
-        });
-
-        app.listen(port ,(err)=>{
-
-            if(err)
-
-             throw err;
-
-            console.log('my server is running on port : ${3000}');
-
-        });
